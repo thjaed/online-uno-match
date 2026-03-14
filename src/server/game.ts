011ce2ed5f1d34189ecdb4e868405845567a7390
thjaed <thomasjedwards10@gmail.com>
2026-03-14 23:40:27 +0000

    implemented card effects

--- src/server/game.ts
+++ src/server/game.ts
@@ -8,6 +8,7 @@ export class Game {
     discard: Card[]
     currentPlayerIndex: number
     direction: 1 | -1
+    colour_effect: Colour | null
 
     constructor(players: Player[]) {
         this.players = players
@@ -15,6 +16,11 @@ export class Game {
         this.discard = []
         this.currentPlayerIndex = 0
         this.direction = 1
+        this.colour_effect = null
+    }
+
+    getTopCard() {
+        return this.discard[this.discard.length - 1]!
     }
 
     startGame() {
@@ -29,27 +35,30 @@ export class Game {
         this.direction = 1
     }
 
-    nextPlayer() {
-        if ((this.currentPlayerIndex + this.direction) <= (this.players.length - 1)) {
-            this.currentPlayerIndex += this.direction
-        } else {
-            this.currentPlayerIndex = 0
-        }
+    getNextPlayer(index?: number, increment: number = 1) {
+        index ??= this.currentPlayerIndex
+        
+        const step = increment * this.direction
+        const next = (index + step + this.players.length) % this.players.length
+        
+        return next
     }
 
-    isCardValid(card: Card, top: Card) {
+    isCardValid(card: Card, top?: Card, check_colour_effect?: boolean) {
+        top = top ? top : this.getTopCard()
+
         if (card.type === "wild" || top.type == "wild") { //is wild card
             return true
         } else if (card.value === top.value) { // is same value
             return true
-        } else if (card.colour == top.colour) { // is same colour
+        } else if (card.colour === (check_colour_effect ? this.colour_effect : top.colour)) { // is same colour as top or as colour effect if being checked
             return true
         } else {
             return false
         }
     }
-    
-    placeCard(player_id: number, hand_index: number) {
+
+    placeCard(player_id: number, hand_index: number, chosen_colour?: Colour) {
         const player = this.players.find(x => x.id === player_id)
         if (player === undefined) {
             throw new Error("Player not found")
@@ -58,20 +67,63 @@ export class Game {
         if (this.players[this.currentPlayerIndex] !== player) {
             throw new Error("Not player's turn")
         }
-        
+
         if (hand_index < 0 || hand_index >= player.hand.length) {
             throw new Error("Hand index out of bounds")
         }
+
         const card = player.hand[hand_index]!
 
-        if (!(this.isCardValid(card, this.discard[this.discard.length - 1]!))) {
+        if (!(this.isCardValid(card))) {
             throw new Error("Card not valid")
+        }
 
+        // move card to discard pile
+        player.hand.splice(hand_index, 1)
+        this.discard.push(card)
+
+        this.useEffect(card, chosen_colour)
+
+        if (card.value === "skip") {
+            // skip next player
+            console.log(`Skipping ${this.getNextPlayer()}, Next Player: ${this.getNextPlayer(this.getNextPlayer())}`)
+            this.currentPlayerIndex = this.getNextPlayer(this.getNextPlayer())
         } else {
-            player.hand.splice(hand_index, 1)
-            this.discard.push(card)
-            this.nextPlayer()
-            return player.hand
+            this.currentPlayerIndex = this.getNextPlayer()
+        }
+
+        return player.hand
+    }
+
+    useEffect(card: Card, chosen_colour?: Colour) {
+        // card effects
+        if (card.type === "action" && card.value == "reverse") {
+            // reverse direction
+            this.direction *= -1
+            console.log(`Reversed direction to ${this.direction}`)
+
+        } else if (card.value == "draw2" || card.value == "wild_draw4") {
+            console.log(`Hand before draw: ${JSON.stringify(this.players[this.getNextPlayer()]!.hand)}`)
+            // draw
+            const qty = card.value == "draw2" ? 2 : 4 // set draw quantity
+
+            for (let i = 0; i < qty; i++) {
+                // give next player a card
+                this.players[this.getNextPlayer()]!.hand.push(this.deck.pop()!)
+            }
+
+            console.log(`Player ID ${this.players[this.getNextPlayer()]!.id} drew ${qty}`)
+            console.log(`Hand after draw: ${JSON.stringify(this.players[this.getNextPlayer()]!.hand)}`)
+        }
+
+        if (card.type == "wild") {
+            // change colour
+            if (chosen_colour) {
+                console.log(`Setting colour to ${chosen_colour}`)
+                this.colour_effect = chosen_colour
+            } else {
+                throw new Error("No colour specified")
+            }
         }
     }
 }
